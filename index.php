1f47d0cc0a68e5f572d830c8659660322dc1fa70
truonghanh90 <truonghanhhv1990@gmail.com>
2020-03-06 16:20:18 +0700

    apply stask apply

--- index.php
+++ index.php
@@ -1,4 +1,14 @@
 <?php
     echo 'test data';
     echo 'data of branch2';
+    echo '123';
+    echo 'revert2';
+    echo 'git reset commit';
+    echo 'hoa';
+    echo 'hong';
+    echo "du lieu them vao ne";
+    echo "hoa bang lang";
+    echo "du lieu them vao master ne";
+    echo 'du lieu stash';
+    echo "du lieu stash2";
 ?>
