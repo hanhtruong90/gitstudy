3fc5ad856ec19fe29d3295311ed53ab41256fd58
truonghanh90 <truonghanhhv1990@gmail.com>
2020-03-09 09:13:47 +0700

    update index

--- index.php
+++ index.php
@@ -1,27 +1,3 @@
 <?php
-    echo 'test data';
-    echo '123';
-    echo 'revert2';
-    echo 'git reset commit';
-    echo 'hoa';
-    echo 'hong';
-    echo "du lieu them vao ne";
-    echo "hoa bang lang";
-    echo "du lieu them vao master ne";
-    echo 'du lieu stash';
-    echo "du lieu stash2";
-    echo "commit 1";
-    echo "commit 2";
-    echo 'data of branch2';
-    echo '123';
-    echo 'revert2';
-    echo 'git reset commit';
-    echo 'hoa';
-    echo 'hong';
-    echo "du lieu them vao ne";
-    echo "hoa bang lang";
-    echo "du lieu them vao master ne";
-    echo 'du lieu stash';
-    echo "du lieu stash2";
-    echo 'du lieu diff';
+    echo 'testne';
 ?>
